23f98514f6039b3135c1e3365b161485d99d81b9
venkyChu <223877@qq.com>
2016-01-10 20:44:18 +0800

    init

---
+++ src/com/example/gitdemo/aa/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.gitdemo.aa;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+
+}
